f4a0651c6ab7bf3862369628d632230bd8891a36
Venktesh <venkteshnirmalkar@gmail.com>
2024-08-07 14:50:53 +0530

    first commit

---
+++ Dtl.java
@@ -0,0 +1,20 @@
+class Dtl{
+	public static void main(String args[]){
+	
+                int age=22;
+		long phno=91134592990l;
+		String gender="Male";
+		String dbo="11-10-2000";
+
+//		System.out.println("\t The age of : " +age);
+//		System.out.println("\t Phone no is : " +phno);
+//		System.out.println("\t Gender is : " +gender);
+//		System.out.println("\t Date of Birth is : " +dbo);
+
+
+		System.out.println("age is : " +age+ "  Ram" );
+		System.out.println(phno);
+		System.out.println(gender);
+		System.out.println(dbo);
+	}
+}
